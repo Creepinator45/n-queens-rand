0074af446e2f0485a6a7b25b3f4eb8ffe753346c
T <creepinator45@gmail.com>
2026-04-09 19:33:36 -0700

    rewrote compute collisions to be cleaner and more closely follow the reference paper

--- n-queens-rand.py
+++ n-queens-rand.py
@@ -10,23 +10,23 @@ def compute_collisions(queen,dn,dp)->int:
     size = len(queen)
     # count queens on positive diagonals 
     # using zip(range) instead of enumerate to maintain symmetry with negative diagonals
-    for (i, element) in  zip(range(size), queen):
-        diag_offset = element-i
-        # offsetting by size to make all indices positive
-        diag_offset += size-1
-        dp[diag_offset] += 1
-    # count queens on negative diagonals
-    for (i, element) in  zip(reversed(range(size)), queen):
-        diag_offset = element-i
-        # offsetting by size to make all indices positive
-        diag_offset += size-1
-        dn[diag_offset] += 1
+    for (row_index, col_index) in  enumerate(queen):
+        # queens that share a negative diagonal have the same sum
+        sum = row_index + col_index
+        dn[sum] += 1
+        # queens that share a positive diagonal have the same difference
+        # offset to give positive indices
+        offset_dif = row_index-col_index + size-1
+        dp[offset_dif] += 1
     # count total collisions
     collisions = 0
     for diagonal in dn + dp:
         if diagonal >= 2:
             collisions += diagonal-1
     return collisions
+
+def compute_attacks(queen, dn, np, attack) -> int:
+    pass    
 
 def queen_search2(queen, C1 = 0.45, C2 = 32):
     size = len(queen)
@@ -41,7 +41,6 @@ def queen_search2(queen, C1 = 0.45, C2 = 32):
     limit = C1*collisions
     #number_of_attacks = compute_attacks(queen, dn, np, attack)
 
-#def compute_attacks(queen, dn, np, attack) -> int:
 def main():
     queen = init_queens(8)
     print(queen)
